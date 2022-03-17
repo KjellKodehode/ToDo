e3b2968598f994610673aaafe01504404dc92607
KjellKodeHode <kjell.arne.kodehode@gmail.com>
2022-03-17 13:19:31 +0100

    new type

--- script.js
+++ script.js
@@ -1,54 +1,30 @@
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+let date = new Date();
+
+document.querySelector('button').addEventListener('click', () =>{
+    addToDo();
+})
+
+function addToDo(){
+    if(!input.value){
+        alert("Enter something to do !")
+    }else{
+        const li = document.createElement('li');
+        li.classList.add('flex');
+        const toDo = document.createElement('p');
+        toDo.textContent = input.value + " <-%-> " + date.toDateString();
+        li.append(toDo);
+        list.append(li);
+        addDeleteButton(li);
+        input.value="";
+    }
 }
 
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-let list = document.querySelector("ul");
-list.addEventListener("click", function(ev) {
-    if (ev.target.tagName === "LI") {
-        ev.target.classList.toggle("checked");
-    }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === "") {
-        alert("You need to type what you want to do !");
-    }else {
-        document.getElementById("myUL").appendChild(li);
-    }
-
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
+function addDeleteButton(li){
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    deleteButton.classList.add('btn', 'btn__delete');
+    li.append(deleteButton);
+    deleteButton.addEventListener('click', ()=> deleteButton.parentNode.remove());
 }
